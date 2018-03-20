962fc3d67a77113caac4a87985d4ba8ebd163053
kit10000000 <ekaterina0196@yandex.ru>
2018-03-20 21:21:35 +0300

    1 project's commit

---
+++ AVLTree/examples/example2.cpp
@@ -0,0 +1,9 @@
+//
+//  example2.cpp
+//  AVLTree
+//
+//  Created by E. Chernikova on 20.03.2018.
+//  Copyright © 2018 E. Chernikova. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ AVLTree/sources/tree.cpp
@@ -0,0 +1,46 @@
+//
+//  tree.cpp
+//  AVLTree
+//
+//  Created by E. Chernikova on 20.03.2018.
+//  Copyright © 2018 E. Chernikova. All rights reserved.
+//
+
+#include <stdio.h>
+#include "sources/tree.h"
+
+AVLTree::Tree::Tree()
+{
+    this -> root = nullptr;
+}
+AVLTree::Tree::~Tree()
+{
+
+}
+void AVLTree::Tree::insert(int key)
+{
+     this->root=insert(key, this->root);
+
+}
+
+AVLTree::Node* AVLTree::Tree::insert(int key,AVLTree::Node* node)
+{
+    if(node == nullptr)
+    {
+        Node* node = new Node();
+        node -> data = key;
+        node -> right = nullptr;
+        node -> left  = nullptr;
+        return node;
+    }
+    else
+    {
+        if(key< node ->data)
+            node->left = insert(key, node->left);
+        else
+        {
+            node->right = insert(key, node->right);
+        }
+    }
+    return node;
+}
